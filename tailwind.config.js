45c7ed99c0cf8909506d7cd941dc82969f50da75
Leonardo Souza <leogsouza@gmail.com>
2024-04-11 15:25:13 -0300

    Added form section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '16': '16rem'
+      }
+    },
   },
   plugins: [],
 }
